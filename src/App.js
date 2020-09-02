795a9276dc2901774955555cb848f1517d445506
kashifrazabb <kashifrazabb@gmail.com>
2020-09-02 23:34:44 +0500

    configure testing with enzyme

--- src/App.js
+++ src/App.js
@@ -1,47 +1,15 @@
-import React, { useEffect, useState, useReducer } from 'react';
-import axios from 'axios';
-import Select from './Select';
-import CountUp from 'react-countup';
+import React, { createContext, useReducer } from 'react';
+import Child from './Child';
+import {reducer} from './reducers/reducer'
+
+export const stateContext = createContext()
 
 const App = () => {
-    const [countries,setCountries]=useState();
-    const [data,setData]=useState();
-    const [global,setGlobal]=useState();
-    const [date,setDate]=useState();
-    const [selectValue,setSelectValue]=useState('Global');
-
-    const handleSelect=(e)=>setSelectValue(e.target.value)
-
-    useEffect(()=>{
-        (async ()=>{
-            const DATA=(await axios.get('https://api.covid19api.com/summary')).data;
-            setCountries(DATA.Countries.map(c=>c.Country))
-            setData(DATA.Countries)
-            setGlobal(DATA.Global)
-            setDate(DATA.Date)
-        })()
- 
-    },[])
-
-    if (data){
-        const INDEX=data.findIndex(i=>i.Country===selectValue);
-        if (selectValue!=='Global'){
-            var {TotalConfirmed,TotalRecovered,TotalDeaths}=data[INDEX];
-        }
-        else if (selectValue==='Global' && global){
-            var {TotalConfirmed,TotalRecovered,TotalDeaths}=global;
-        }
-    }
-
+    const [stateCounter,dispatchCounter]=useReducer(reducer,0)
     return (
-        <>
-            <h1>Confirmed: {TotalConfirmed && <CountUp end={TotalConfirmed} separator=',' />} </h1>
-            <h1>Recovered: {TotalRecovered && <CountUp end={TotalRecovered} separator=','/>} </h1>
-            <h1>Deaths: {TotalDeaths && <CountUp end={TotalDeaths} separator=','/>} </h1>
-            <h1>Last Updated: {date && new Date(date).toDateString()} </h1>
-            <Select countries={countries?countries:['Loading....']} handleSelect={handleSelect} 
-            selectValue={selectValue}/>
-        </>
+        <stateContext.Provider value={{stateCounter,dispatchCounter}}>
+            <Child />
+        </stateContext.Provider>
     )
 }
 
